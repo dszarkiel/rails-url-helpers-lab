7397056b7b623ce68d5bddb8b94e18d6461c0ac4
dszarkiel <d.szarkiel@gmail.com>
2020-11-21 12:11:56 -0600

    commit tree 65f7233dc2144c2fb96271a3bfdea9db85919ab0

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+student1 = Student.create(first_name: "Dom", last_name: "Sszar")
+student2 = Student.create(first_name: "John", last_name: "Doe")
